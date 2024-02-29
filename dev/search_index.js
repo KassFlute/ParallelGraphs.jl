7252b5b2203278e7708cf4cd9f8f513f917a9c08
Documenter.jl <documenter@juliadocs.github.io>
2024-02-29 18:10:45 +0000

    build based on 63177a4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ParallelGraphs","category":"page"},{"location":"#ParallelGraphs","page":"Home","title":"ParallelGraphs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ParallelGraphs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ParallelGraphs]","category":"page"}]
+}
