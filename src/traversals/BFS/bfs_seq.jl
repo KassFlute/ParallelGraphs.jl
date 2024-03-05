acbf77a1f40708ac1655d7be3a5da0c4fb452498
KassFlute <cassien.roth@epfl.ch>
2024-03-05 14:21:00 +0100

    Simple parallel BFS implemented

--- src/traversals/BFS/bfs_seq.jl
+++ src/traversals/BFS/bfs_seq.jl
@@ -1,5 +1,5 @@
 """
-    bfs(graph::AbstractGraph, source::Integer)
+    bfs_seq(graph::AbstractGraph, source::Integer)
 
 Perform a breadth-first search on `graph` starting from vertex `source`.
 Return a vector of vertices in the order they were visited.
@@ -11,10 +11,10 @@ add_edge!(g, 1, 2)
 add_edge!(g, 2, 3)
 add_edge!(g, 3, 4)
 add_edge!(g, 4, 1)
-bfs(g, 1) # returns [1, 2, 4, 3]
+bfs_seq(g, 1) # returns [1, 2, 4, 3]
 ```
 """
-function bfs(graph::AbstractGraph, source::Integer)
+function bfs_seq(graph::AbstractGraph, source::Integer)
     queue = [source] # FIFO of vertices to visit
     visited = Set([source]) # Set of visited vertices
     visited_order = [source] # Order of visited vertices
