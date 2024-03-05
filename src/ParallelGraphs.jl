663257347c5831b332751e6b23ccbcf0e29080b1
KassFlute <cassien.roth@epfl.ch>
2024-03-05 10:49:12 +0100

    better folder structure

--- src/ParallelGraphs.jl
+++ src/ParallelGraphs.jl
@@ -3,7 +3,7 @@ module ParallelGraphs
 using Graphs: vertices, edges, AbstractGraph, SimpleGraph, neighbors, add_edge!
 
 include("fichier_bidon.jl")
-include("traversals/sequential/bfs.jl")
+include("traversals/BFS/bfs_seq.jl")
 
 export return_true, return_false, bfs, SimpleGraph, add_edge!
 
