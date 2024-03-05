9718df11d05cfc74e76feed5e3b1067f575dacd7
KassFlute <cassien.roth@epfl.ch>
2024-03-05 14:41:24 +0100

    typo in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,13 +28,13 @@ using Test
             graph = SimpleGraph(adjacency_matrix)
 
             expected_order = [1, 2, 3, 4]
-            @test bfs(graph, 1) == expected_order
+            @test bfs_seq(graph, 1) == expected_order
             expected_order = [2, 1, 4, 3]
-            @test bfs(graph, 2) == expected_order
+            @test bfs_seq(graph, 2) == expected_order
             expected_order = [3, 1, 4, 2]
-            @test bfs(graph, 3) == expected_order
+            @test bfs_seq(graph, 3) == expected_order
             expected_order = [4, 2, 3, 1]
-            @test bfs(graph, 4) == expected_order
+            @test bfs_seq(graph, 4) == expected_order
         end
 
         @testset "BFS parallel" begin
