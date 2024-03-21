59bf7ccd0a4a69d1d82bb5274b58c2be58a93139
AntoineBut <90978814+AntoineBut@users.noreply.github.com>
2024-03-21 14:17:29 +0100

    Ab/better parallel bfs (#29)
    * New implementation with local queues
    
    It is up and running ,needs to be profiled to check for inneficiencies and need to solve compat issues with OhMyThreads
    
    * fixed compat
    
    * new versions
    
    lot of things to benchmark yay
    
    * fixezd
    
    * fixed and optimized the 3 versions
    
    * fixed merge

--- src/traversals/BFS/bfs_par.jl
+++ src/traversals/BFS/bfs_par.jl
@@ -52,16 +52,6 @@ function bfs_par_local!(
             end
         end
 
-        function local_exploration!(src::T) where {T<:Integer}
-            for n in neighbors(graph, src)
-                # If the parent is 0, replace it with src vertex and push to queue
-                old_val = atomic_cas!(parents[n], 0, src)
-                if old_val == 0
-                    enqueue!(queues[Threads.threadid()], n)
-                end
-            end
-        end
-
         to_visit = Vector{T}()
         push!(to_visit, source)
 
@@ -80,7 +70,7 @@ function bfs_par_local!(
 end
 
 function bfs_par_local2!(
-    graph::AbstractGraph, source::T, parents::Array{Atomic{T}}, queues::Vector{Queue{T}}
+    graph::AbstractGraph, source::T, parents::Array{Atomic{T}}, queues::Channel{Queue{T}}
 ) where {T<:Integer}
     if source > nv(graph) || source < 1
         throw(ArgumentError("source vertex is not in the graph"))
@@ -91,9 +81,9 @@ function bfs_par_local2!(
             # If the parent is 0, replace it with src vertex and push to queue
             old_val = atomic_cas!(parents[n], 0, src)
             if old_val == 0
-                id = take!(chnl)
-                enqueue!(queues[id], n)
-                put!(chnl, id)
+                q = take!(queues)
+                enqueue!(q, n)
+                put!(queues, q)
             end
         end
     end
@@ -101,20 +91,16 @@ function bfs_par_local2!(
     to_visit = Vector{T}()
     push!(to_visit, source)
 
-    chnl = Channel{Int}(Threads.nthreads())
-    for i in 1:Threads.nthreads()
-        put!(chnl, i)
-    end
-
     parents[source] = Atomic{Int}(source)
-
     while !isempty(to_visit)
         tforeach(local_exploration!, to_visit) # explores vertices in parallel
         to_visit = Vector{T}()
         for i in 1:Threads.nthreads()
-            while !isempty(queues[i])
-                push!(to_visit, dequeue!(queues[i]))
+            q = take!(queues)
+            while !isempty(q)
+                push!(to_visit, dequeue!(q))
             end
+            put!(queues, q)
         end
     end
     return nothing
@@ -151,22 +137,6 @@ function bfs_par_local3!(
     return nothing
 end
 
-#function bfs_par(graph::AbstractGraph, source::T) where {T<:Integer}
-#    if nv(graph) == 0
-#        return T[]
-#    end
-#    queues = Vector{Queue{T}}(undef, Threads.nthreads())
-#    for i in 1:Threads.nthreads()
-#        queues[i] = Queue{T}()
-#    end
-#    parents_atomic = [Atomic{T}(0) for _ in 1:nv(graph)]
-#    bfs_par_local!(graph, source, parents_atomic, queues)
-#
-#    parents = Array{T}(undef, length(parents_atomic))
-#    parents = [x[] for x in parents_atomic]
-#    return parents
-#end
-
 """
     bfs_par_tree(graph::AbstractGraph, source::T)
 
@@ -178,11 +148,27 @@ function bfs_par(graph::AbstractGraph, source::T) where {T<:Integer}
     if nv(graph) == 0
         return T[]
     end
-    channel = Channel{T}(nv(graph))
+    queues = Channel{Queue{T}}(Threads.nthreads())
+    for i in 1:Threads.nthreads()
+        put!(queues, Queue{T}())
+    end
     parents_atomic = [Atomic{T}(0) for _ in 1:nv(graph)]
-    bfs_par_local3!(graph, source, parents_atomic, channel)
+    bfs_par_local2!(graph, source, parents_atomic, queues)
 
     parents = Array{T}(undef, length(parents_atomic))
     parents = [x[] for x in parents_atomic]
     return parents
 end
+
+#function bfs_par(graph::AbstractGraph, source::T) where {T<:Integer}
+#    if nv(graph) == 0
+#        return T[]
+#    end
+#    channel = Channel{T}(nv(graph))
+#    parents_atomic = [Atomic{T}(0) for _ in 1:nv(graph)]
+#    bfs_par_local3!(graph, source, parents_atomic, channel)
+#
+#    parents = Array{T}(undef, length(parents_atomic))
+#    parents = [x[] for x in parents_atomic]
+#    return parents
+#end
