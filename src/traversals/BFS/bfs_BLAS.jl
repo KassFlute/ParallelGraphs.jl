0c84a0baa40d9e0e3fbb30b83e7a284362f3c44f
Antoine Buttier <antoine.buttier@epfl.ch>
2024-04-20 13:15:16 +0200

    meh

--- src/traversals/BFS/bfs_BLAS.jl
+++ src/traversals/BFS/bfs_BLAS.jl
@@ -11,35 +11,46 @@ function bfs_BLAS(graph::AbstractGraph, source::T) where {T<:Integer}
     n = nv(graph)
     p = GBVector{T}(n; fill=zero(T))
     f = GBVector{T}(n; fill=zero(T))
-    #A = permutedims(adjacency_matrix(graph), (2, 1))
-    A = GBMatrix(adjacency_matrix(graph))
-
-    bfs_BLAS!(A', source, p, f)
+    A_T = GBMatrix{T}(adjacency_matrix(graph; dir=:in))
+    bfs_BLAS!(A_T, source, p, f)
 
     return Array(p)
 end
 
 function bfs_BLAS!(
-    A::GBArrayOrTranspose{T}, source::T, p::GBVector{T}, f::GBVector{T}
+    A_T::GBMatrix{T}, source::T, p::GBVector{T}, f::GBVector{T}
 ) where {T<:Integer}
-
-    #mask = GBVector{T}(n ; fill = zero(T))
-
     p[source] = source
     f[source] = source
-    z = GBVector{T}(length(p); fill=zero(T))
-    depth = 0
+    temp = GBVector{T}(length(p); fill=zero(T))
     for _ in 1:length(p)
-        depth += 1
-        mul!(f, A, f, (any, secondi); mask=~p)
-        extract!(f, z, 1:length(p); mask=p)
-        extract!(p, f, 1:length(p); mask=~p)
-        apply!(rowindex, f, f)
+        empty!(temp)
+        mul!(temp, A_T, f, (any, secondi); mask=~p)
+        extract!(p, temp, 1:length(p); mask=temp)
+        empty!(f)
+        apply!(rowindex, f, temp; mask=temp)
         if reduce(max, f) == 0
-            println("BFS ended at depth: ", depth)
             return nothing
         end
     end
-    println("BFS at N : this is bad!")
     return nothing
+
+    #p[source] = source
+    #f[source] = source
+    #z = GBVector{T}(length(p); fill=zero(T))
+    #temp = GBMatrix{T}(1 , length(p); fill=zero(T))
+    #
+    #depth = 0
+    #for _ in 1:length(p)
+    #    depth += 1
+    #    empty!(temp)
+    #    mul!(temp, f', A, (any, secondi); mask=~p')
+    #    #extract!(f, z, 1:length(p); mask=p)
+    #    extract!(p, temp, 1:length(p); mask=temp)
+    #    apply(rowindex, f, temp')
+    #    if reduce(max, f) == 0
+    #        return nothing
+    #    end
+    #end
+    #return nothing
 end
