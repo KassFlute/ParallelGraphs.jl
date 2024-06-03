d6538dbcfde3e68f8e522a1b280dff612f31ab69
KassFlute <cassien.roth@epfl.ch>
2024-06-03 16:40:52 +0200

    Reworked benchmarks (#57)
    * Update packages
    
    * Add DataFrames.jl
    
    * Add CSV.jl
    
    * Ignore benchmark CSV DataFrame
    
    * Add Plots
    
    * Ignore benchmark.csv
    
    * gitignore all benchmark outputs
    
    * Rework all benchmarks
    
    * Store benchmarks outputs in DataFrame
    
    * Add benchmark outputs plotting
    
    * Automate benchmark run
    
    * Update Manifest.toml
    
    * Add imported graph
    
    * Move graph sizes to top of file
    
    * Update plots
    
    * Ignore bib file
    
    * Update Manifest
    
    * Fix typo
    
    * Make both axis log
    
    * Format
    
    * Fix prints
    
    * Refactor Graphs types names
    
    * Make y axis linear again
    
    * Fix prints
    
    * Add more sizes
    
    * Format
    
    * Update Project.toml
    
    * Add separate file for full local benchmarking
    
    * Add bigger grpahs to bench
    
    * Add coloring benchmarks
    
    * Format
    
    * Add graph.jl coloring bench
    
    * Format
    
    * Add BLAS coloring bench
    
    * Format

---
+++ benchmark/bench_local.jl
@@ -0,0 +1,15 @@
+include("benchmarks.jl")
+
+print("Run benchmarks...")
+results = run(SUITE)
+println("OK")
+
+print("Parse results...")
+data = parse_results(results)
+colors = parse_colors()
+println("OK")
+
+println("Plot results...")
+plot_BFS_results(data)
+plot_coloring_results(data, colors)
+println("FINISH")
--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -12,7 +12,9 @@ using Graphs:
     grid,
     path_digraph,
     AbstractGraph,
-    adjacency_matrix
+    adjacency_matrix,
+    degree_greedy_color,
+    greedy_color
 using SuiteSparseGraphBLAS:
     GBVector, GBMatrix, setstorageorder!, RowMajor, mul!, extract!, gbset, format, Monoid
 import Graphs.Parallel as GP
@@ -22,10 +24,16 @@ using GraphIO.EdgeList
 using GraphIO.EdgeList: IntEdgeListFormat, loadgraph
 using ParserCombinator
 using GraphIO.GML: GMLFormat
+using DataFrames
+using CSV
+using Plots
+using Statistics: mean
 
 #######################
 ### Benchmark setup ###
 #######################
+
+SIZES_TO_GENERATE = [10, 100, 1000, 10000, 100000] # sizes in number of vertices
 
 # BenchmarkTools parameters
 BenchmarkTools.DEFAULT_PARAMETERS.samples = 10
@@ -41,66 +49,57 @@ else
     @warn "Julia started with: $(Threads.nthreads()) threads."
 end
 
-# Benchmark graphs parameters
-SIZE = [30_000, 300_000, 3_000_000] # sizes in number of vertices
-CLASSES = [
-    "30k",
-    "300k",
-    "3M",
-    "twitch_user",
-    #"live journal",
-]
+##################
+### Add Graphs ###
+##################
 
-generated_graphs = [Vector{AbstractGraph{Int}}() for _ in 1:length(SIZE)]
-g_first_vertex = [Vector{Int}() for _ in 1:length(SIZE)]
-
-imported_graphs = Vector{AbstractGraph{Int}}()
-i_first_vertex = Vector{Int}()
-
-names = Dict{String,Vector{String}}("Generated" => [], "Imported" => [])
-
-function addgraphtolist(i::Int, g::AbstractGraph{Int}, n::String, v::Int)
-    push!(generated_graphs[i], g)
-    push!(names["Generated"], n)
-    return push!(g_first_vertex[i], v)
+# Struct to store the graphs to benchmark
+@enum GraphType begin
+    GEN
+    IMPORT
 end
 
-for i in eachindex(SIZE)
-    v = SIZE[i]
+struct BenchGraphs
+    graph::AbstractGraph
+    size::Int
+    name::String
+    type::GraphType
+    start_vertex::Int
+end
 
-    #addgraphtolist(i, dorogovtsev_mendes(v), "dorogovtsev_mendes", 1)
-    addgraphtolist(i, barabasi_albert(v, 2), "barabasi_albert - 2", 1)
-    addgraphtolist(i, barabasi_albert(v, 8), "barabasi_albert - 8", 1)
-    addgraphtolist(i, binary_tree(round(Int, log2(v)) + 1), "binary_tree", 1)
-    #addgraphtolist(i, double_binary_tree(round(Int, log2(v))), "double_binary_tree", 1)
-    addgraphtolist(i, star_graph(v), "star_graph - center start", 1)
-    addgraphtolist(i, star_graph(v), "star_graph - border start", 2)
+# Generate bench graphs
+SIZES = SIZES_TO_GENERATE # sizes in number of vertices
+bench_graphs = Vector{BenchGraphs}()
+print("Generate graphs...")
+for i in eachindex(SIZES)
+    v = SIZES[i]
+    push!(bench_graphs, BenchGraphs(dorogovtsev_mendes(v), v, "dorogovtsev_mendes", GEN, 1))
+    push!(
+        bench_graphs, BenchGraphs(barabasi_albert(v, 2), v, "barabasi_albert - 2", GEN, 1)
+    )
+    push!(
+        bench_graphs, BenchGraphs(barabasi_albert(v, 8), v, "barabasi_albert - 8", GEN, 1)
+    )
+    push!(
+        bench_graphs,
+        BenchGraphs(binary_tree(round(Int, log2(v)) + 1), v, "binary_tree", GEN, 1),
+    )
+    #push!(generated_graphs, BenchGraphs(double_binary_tree(round(Int, log2(v))), "double_binary_tree", GENERATED, 1))
+    push!(bench_graphs, BenchGraphs(star_graph(v), v, "star_graph - center start", GEN, 1))
+    push!(bench_graphs, BenchGraphs(star_graph(v), v, "star_graph - border start", GEN, 2))
     N = round(Int, sqrt(sqrt(v)))
-    addgraphtolist(i, grid([N, N, N, N]), "grid 4 dims", 1)
-    #addgraphtolist(i, path_digraph(v), "path_digraph", round(Int, v / 2))
+    push!(bench_graphs, BenchGraphs(grid([N, N, N, N]), v, "grid 4 dims", GEN, 1))
+    #push!(generated_graphs, BenchGraphs(path_digraph(v), "path_digraph", GENERATED, round(Int, v / 2)))
 end
+println("OK")
 
-# Load graphs from files
-
-#push!(imported_graphs, loadgraph("benchmark/data/routers.csv", "routers", EdgeListFormat()))
-#push!(names["Imported"], "routers.csv")
-#push!(i_first_vertex, 1)
-#
-#push!(
-#    imported_graphs,
-#    loadgraph("benchmark/data/internet_routers_bigger.gml", "graph", GMLFormat()),
-#)
-#push!(names["Imported"], "internet_routers_bigger.gml")
-#push!(i_first_vertex, 1)
-
-push!(
-    imported_graphs,
-    loadgraph(
-        "benchmark/data/large_twitch_edges.csv", "twitch user network", EdgeListFormat()
-    ),
-)
-push!(names["Imported"], "large_twitch_edges.csv")
-push!(i_first_vertex, 1)
+# Add imported graphs
+print("Import graphs...")
+# g = loadgraph(
+#     "benchmark/data/large_twitch_edges.csv", "twitch user network", EdgeListFormat()
+# )
+# push!(bench_graphs, BenchGraphs(g, nv(g), "large_twitch_edges.csv", IMPORT, 1))
+println("OK")
 
 #push!(
 #    imported_graphs,
@@ -117,11 +116,11 @@ push!(i_first_vertex, 1)
 
 Create a benchmark for BFS on a graph `g` with a starting vertex `v` and store it in the global `SUITE` variable.
 """
-function bench_BFS(g::AbstractGraph, v::Int, name::String, class::String)
+function bench_BFS(bg::BenchGraphs)
     # Our sequential
-    SUITE["BFS"][class][name]["seq"] = @benchmarkable ParallelGraphs.bfs_seq!(
-        $g, $v, parents_prepared
-    ) evals = 1 setup = (parents_prepared = fill(0, nv($g)))
+    SUITE["BFS"][string(bg.type) * ": " * bg.name][bg.size]["seq"] = @benchmarkable ParallelGraphs.bfs_seq!(
+        $bg.graph, $bg.start_vertex, parents_prepared
+    ) evals = 1 setup = (parents_prepared = fill(0, nv($bg.graph)))
 
     ## Our parallel similar to Graphs.jl
     #SUITE["BFS"][class][name]["par"] = @benchmarkable ParallelGraphs.bfs_par!(
@@ -129,68 +128,250 @@ function bench_BFS(g::AbstractGraph, v::Int, name::String, class::String)
     #) evals = 1 setup = (parents_atomic_prepared = [Atomic{Int}(0) for _ in 1:nv($g)])
 
     # Our parallel with local queues
-    SUITE["BFS"][class][name]["par_local"] = @benchmarkable ParallelGraphs.bfs_par_local!(
-        $g, $v, parents_atomic_prepared, queues_prepared, to_visit_prepared
-    ) evals = 1 setup = (parents_atomic_prepared = [Atomic{Int}(0) for _ in 1:nv($g)];
-    queues_prepared = Vector{Queue{Int}}();
-    foreach(1:(10 * Threads.nthreads())) do i
-        push!(queues_prepared, Queue{Int}())
-    end;
-    to_visit_prepared = zeros(Int, nv($g)))
+    SUITE["BFS"][string(bg.type) * ": " * bg.name][bg.size]["par_local"] = @benchmarkable ParallelGraphs.bfs_par_local!(
+        $bg.graph,
+        $bg.start_vertex,
+        parents_atomic_prepared,
+        queues_prepared,
+        to_visit_prepared,
+    ) evals = 1 setup = (
+        parents_atomic_prepared = [Atomic{Int}(0) for _ in 1:nv($bg.graph)];
+        queues_prepared = Vector{Queue{Int}}();
+        foreach(1:(10 * Threads.nthreads())) do i
+            push!(queues_prepared, Queue{Int}())
+        end;
+        to_visit_prepared = zeros(Int, nv($bg.graph))
+    )
 
     ## Our GraphBLAS based implementation
-
-    A_T = GBMatrix{Bool}((adjacency_matrix(graph, Bool; dir=:in)))
-    SUITE["BFS"][class][name]["BLAS"] = @benchmarkable ParallelGraphs.bfs_BLAS!(
-        $A_T, $v, p, f
-    ) evals = 1 setup = (p = GBVector{Int}(nv($g); fill=zero(Int));
-    f = GBVector{Bool}(nv($g); fill=false))
+    A_T = GBMatrix{Bool}((adjacency_matrix(bg.graph, Bool; dir=:in)))
+    SUITE["BFS"][string(bg.type) * ": " * bg.name][bg.size]["BLAS"] = @benchmarkable ParallelGraphs.bfs_BLAS!(
+        $A_T, $bg.start_vertex, p, f
+    ) evals = 1 setup = (p = GBVector{Int}(nv($bg.graph); fill=zero(Int));
+    f = GBVector{Bool}(nv($bg.graph); fill=false))
 
     ## Graphs.jl implementation
-    return SUITE["BFS"][class][name]["graphs.jl_par"] = @benchmarkable GP.bfs_tree!(
-        next_prepared, $g, $v, parents_prepared
-    ) evals = 1 setup = (next_prepared = GP.ThreadQueue(Int, nv($g));
-    parents_prepared = [Atomic{Int}(0) for i in 1:nv($g)])
+    SUITE["BFS"][string(bg.type) * ": " * bg.name][bg.size]["graphs.jl_par"] = @benchmarkable GP.bfs_tree!(
+        next_prepared, $bg.graph, $bg.start_vertex, parents_prepared
+    ) evals = 1 setup = (next_prepared = GP.ThreadQueue(Int, nv($bg.graph));
+    parents_prepared = [Atomic{Int}(0) for i in 1:nv($bg.graph)])
+
+    return SUITE
 end
 
-println("Benchmarking BFS on imported graphs : ", length(imported_graphs))
-for i in eachindex(imported_graphs)
-    graph = imported_graphs[i]
-    name = names["Imported"][i]
-    vertex = i_first_vertex[i]
-    class = CLASSES[length(SIZE) + i]
-    print(". ")
-    bench_BFS(graph, vertex, name, class)
+print("Add BFS benchmarks...")
+for i in eachindex(bench_graphs)
+    bench_BFS(bench_graphs[i])
 end
-println(" ")
+println("OK (", length(bench_graphs), " added)")
 
-println(
-    "Benchmarking BFS on generated graphs and sizes : ",
-    length(generated_graphs[1]),
-    " x ",
-    length(generated_graphs),
-)
-for s in eachindex(SIZE)
-    for g in eachindex(generated_graphs[s])
-        graph = generated_graphs[s][g]
-        name = names["Generated"][g]
-        vertex = g_first_vertex[s][g]
-        class = CLASSES[s]
-        print(". ")
-        bench_BFS(graph, vertex, name, class)
+##########################
+### benchmark Coloring ###
+##########################
+"""
+    bench_Coloring(g::AbstractGraph, v::Int, name::String, class::String)
+
+Create a benchmark for Coloring on a graph `g` and store it in the global `SUITE` variable.
+"""
+function bench_Coloring(bg::BenchGraphs)
+    # Our degree sequential
+    SUITE["Coloring"][string(bg.type) * ": " * bg.name][bg.size]["seq"] = @benchmarkable ParallelGraphs.degree_order_and_color(
+        $bg.graph
+    ) evals = 1
+
+    # Our random sequential
+    SUITE["Coloring"][string(bg.type) * ": " * bg.name][bg.size]["seq_random"] = @benchmarkable ParallelGraphs.shuffle_and_color(
+        $bg.graph
+    ) evals = 1
+
+    # Our BLAS
+    SUITE["Coloring"][string(bg.type) * ": " * bg.name][bg.size]["BLAS"] = @benchmarkable ParallelGraphs.BLAS_coloring_maxIS(
+        $bg.graph
+    ) evals = 1
+
+    # Graph.jl degree sequential
+    SUITE["Coloring"][string(bg.type) * ": " * bg.name][bg.size]["graphs.jl_seq"] = @benchmarkable degree_greedy_color(
+        $bg.graph
+    ) evals = 1
+
+    # Graph.jl random sequential
+    SUITE["Coloring"][string(bg.type) * ": " * bg.name][bg.size]["graphs.jl_seq_random"] = @benchmarkable greedy_color(
+        $bg.graph
+    ) evals = 1
+
+    return SUITE
+end
+
+print("Add Coloring benchmarks...")
+for i in eachindex(bench_graphs)
+    bench_Coloring(bench_graphs[i])
+end
+println("OK (", length(bench_graphs), " added)")
+
+# Run coloring and save color number
+coloring_sample = 10
+data_colors = DataFrame(; graph=[], size=[], algo_implem=[], color_numbers=[])
+
+for graph in bench_graphs
+    graph_name = string(graph.type) * ": " * graph.name
+
+    color_runs = []
+    for _ in 1:coloring_sample
+        num_colors = ParallelGraphs.degree_order_and_color(graph.graph).num_colors
+        push!(color_runs, num_colors)
     end
-    println(" ")
+    push!(data_colors, (graph_name, graph.size, "seq_degree", color_runs))
+
+    color_runs = []
+    for _ in 1:coloring_sample
+        num_colors = ParallelGraphs.shuffle_and_color(graph.graph).num_colors
+        push!(color_runs, num_colors)
+    end
+    push!(data_colors, (graph_name, graph.size, "seq_random", color_runs))
+
+    color_runs = []
+    for _ in 1:coloring_sample
+        num_colors = ParallelGraphs.BLAS_coloring_maxIS(graph.graph).num_colors
+        push!(color_runs, num_colors)
+    end
+    push!(data_colors, (graph_name, graph.size, "BLAS", color_runs))
+
+    color_runs = []
+    for _ in 1:coloring_sample
+        num_colors = degree_greedy_color(graph.graph).num_colors
+        push!(color_runs, num_colors)
+    end
+    push!(data_colors, (graph_name, graph.size, "graphs.jl_seq", color_runs))
+
+    color_runs = []
+    for _ in 1:coloring_sample
+        num_colors = greedy_color(graph.graph).num_colors
+        push!(color_runs, num_colors)
+    end
+    push!(data_colors, (graph_name, graph.size, "graphs.jl_seq_random", color_runs))
 end
 
-# If a cache of tuned parameters already exists, use it, otherwise, tune and cache
-# the benchmark parameters. Reusing cached parameters is faster and more reliable
-# than re-tuning `suite` every time the file is included.
+##############################
+### benchmarks run methods ###
+##############################
 
-# paramspath = joinpath(dirname(@__FILE__), "params.json")
+function parse_results(results; path="benchmark/out/benchmarks.csv")
+    data = DataFrame(; tested_algo=[], graph=[], size=[], algo_implem=[], minimum_time=[])
+    for tested_algo in identity.(keys(results))
+        for graph in identity.(keys(results[tested_algo]))
+            for size in identity.(keys(results[tested_algo][graph]))
+                for algo_implem in identity.(keys(results[tested_algo][graph][size]))
+                    perf = results[tested_algo][graph][size][algo_implem]
+                    #println(tested_algo, " ", graph, " ", size, " ", algo_implem, " ", perf)
+                    push!(
+                        data, (tested_algo, graph, size, algo_implem, minimum(perf.times))
+                    )
+                end
+            end
+        end
+    end
 
-# if isfile(paramspath)
-#     loadparams!(SUITE, BenchmarkTools.load(paramspath)[1], :evals)
-# else
-#     tune!(SUITE)
-#     BenchmarkTools.save(paramspath, params(SUITE))
-# end
+    if !isnothing(path)
+        dir = dirname(path)
+        mkpath(dir)
+        open(path, "w") do file
+            CSV.write(file, data)
+        end
+    end
+    return data
+end
+
+function parse_colors()
+    return data_colors
+end
+
+function plot_BFS_results(data::DataFrame)
+    markers = [
+        :circle, :cross, :diamond, :dtriangle, :heptagon, :hexagon, :hline, :ltriangle
+    ]
+    data = filter(row -> row.tested_algo == "BFS", data)
+    grouped_by_graph = groupby(data, :graph)
+    for (i, graph_group) in enumerate(grouped_by_graph)
+        p = plot(;
+            title=string(graph_group.graph[1]),
+            xlabel="Size",
+            ylabel="Time (ns)",
+            xscale=:log10,
+        )
+        algo_grouped = groupby(graph_group, :algo_implem)
+
+        for (j, algo_group) in enumerate(algo_grouped)
+            sorted_group = sort(algo_group, :size)
+            plot!(
+                p,
+                sorted_group.size,
+                sorted_group.minimum_time;
+                label=string(sorted_group.algo_implem[1]),
+                seriestype=:line,
+                marker=markers[j],
+            )
+        end
+        filename = "benchmark/out/plot_BFS_$(graph_group.graph[1])_$(i).png"
+        savefig(p, filename)
+        println("Saved plot to $filename")
+    end
+end
+
+function plot_coloring_results(data::DataFrame, colors::DataFrame)
+    markers = [
+        :circle, :cross, :diamond, :dtriangle, :heptagon, :hexagon, :hline, :ltriangle
+    ]
+    data = filter(row -> row.tested_algo == "Coloring", data)
+    grouped_by_graph = groupby(data, :graph)
+    for (i, graph_group) in enumerate(grouped_by_graph)
+        p = plot(;
+            title=string(graph_group.graph[1]),
+            xlabel="Size",
+            ylabel="Time (ns)",
+            xscale=:log10,
+        )
+        algo_grouped = groupby(graph_group, :algo_implem)
+
+        for (j, algo_group) in enumerate(algo_grouped)
+            sorted_group = sort(algo_group, :size)
+            plot!(
+                p,
+                sorted_group.size,
+                sorted_group.minimum_time;
+                label=string(sorted_group.algo_implem[1]),
+                seriestype=:line,
+                marker=markers[j],
+            )
+        end
+        filename = "benchmark/out/plot_Coloring_$(graph_group.graph[1])_$(i)_times.png"
+        savefig(p, filename)
+        println("Saved plot to $filename")
+    end
+
+    data = colors
+    grouped_by_graph = groupby(data, :graph)
+    for (i, graph_group) in enumerate(grouped_by_graph)
+        p = plot(;
+            title=string(graph_group.graph[1]),
+            xlabel="Size",
+            ylabel="Color number",
+            xscale=:log10,
+        )
+        algo_grouped = groupby(graph_group, :algo_implem)
+
+        for (j, algo_group) in enumerate(algo_grouped)
+            sorted_group = sort(algo_group, :size)
+            plot!(
+                p,
+                sorted_group.size,
+                mean.(sorted_group.color_numbers);
+                label=string(sorted_group.algo_implem[1]),
+                seriestype=:line,
+                marker=markers[j],
+            )
+        end
+        filename = "benchmark/out/plot_Coloring_$(graph_group.graph[1])_$(i)_colors.png"
+        savefig(p, filename)
+        println("Saved plot to $filename")
+    end
+end
